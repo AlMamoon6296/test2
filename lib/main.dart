e65454931af8f9f291d8a39c45e315dc81df701d
Mamoon <sayedalmamoon@gmail.com>
2024-12-19 20:43:11 +0530

    ...

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat/pages/homePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
